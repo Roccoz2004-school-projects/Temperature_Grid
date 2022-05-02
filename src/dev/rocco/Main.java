4e5d06d2d325a4c11ebc6834488dbecea7f39c18
ComputerLab <roccoz2004@gmail.com>
2022-05-02 08:37:25 -0700

    Initial commit

---
+++ src/dev/rocco/Main.java
@@ -0,0 +1,14 @@
+package dev.rocco;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] twoDimentional = {{1,5},{2,6},{3,7},{4,8}};
+        for (int i = 0 ; i < 4 ; i++) {
+            for (int j = 0 ; j < 2; j++){
+                System.out.print(twoDimentional[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
